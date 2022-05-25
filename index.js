5a1a8d62df94e2dc628b6e02ac80cbd875e6b03e
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-05-25 19:09:42 -0400

    Adding questions

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const axios = require("axios");
+const markdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Please enter the title of your project'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your GitHub username'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'Please enter your email address'
+    }
+
+];
+
+inquirer
+    .prompt(questions)
+    .then((data) => {
+        console.log(data)
+    })
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
