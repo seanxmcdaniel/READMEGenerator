84ef6d08b4f5c9b102165e6235175239934859cd
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2022-05-28 23:45:30 -0400

    testing again...

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,16 +1,16 @@
 function renderLicenseBadge(license) {
 
   if(license = 'MIT') {
-    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+    badge =  `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
   } else if (license = 'IBM') {
-    return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
+    badge = `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)`
   } else if (license = 'Mozilla') {
-    return `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
-  }else if (license = 'WTFPL') {
-    return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
+    badge = `[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)`
+  } else if (license = 'WTFPL') {
+    badge = `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`
   } else {
-    return " "
-  }
+    badge = ``
+  } return badge;
 };
 
 function renderLicenseLink(license) {
